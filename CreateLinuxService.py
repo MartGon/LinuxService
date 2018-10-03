92377afe7522c0e7bc345f194a28474c7606b5ac
MartGon <gudo35@gmail.com>
2018-10-03 12:01:16 +0200

    Added Working directoroy

--- CreateLinuxService.py
+++ CreateLinuxService.py
@@ -10,12 +10,14 @@ if len(sys.argv) != 2:
 
 execName = os.path.abspath(sys.argv[1])
 serviceFileNameSplitted = os.path.relpath(execName, os.path.dirname(os.path.realpath(sys.argv[1]))).split(".")
+workingDirectory = execName.replace(sys.argv[1], "")
 
 if len(serviceFileNameSplitted) != 2:
 	print 'El archivo no parece tener una terminación válida'
 	quit()
 
 print 'El path absoluto del ejectuable es', execName
+print 'El working directory es', workingDirectory
 
 servicePath = '/etc/systemd/system/'
 serviceName = serviceFileNameSplitted[0] + '.service';
@@ -29,7 +31,7 @@ print 'El path del servicio es', serviceFilePath
 serviceFile = open(serviceFilePath, "w")
 
 serviceFile.write('[Unit] \nDescription=DroneService \nAfter=network.target')
-serviceFile.write('\n[Service] \nExecStart=' + execName)
+serviceFile.write('\n[Service] \nExecStart=' + execName + '\nWorkingDirectory=' + workingDirectory)
 serviceFile.write('\n[Install] \nWantedBy=multi-user.target \n')
 
 serviceFile.close()
